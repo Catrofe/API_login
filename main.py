c123d42e836dcc35c3c2bc572c11e6914d0ee9d4
Catrofe <lord.rico10@gmail.com>
2022-02-18 13:24:14 -0300

    Alterando main e removendo class

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ from typing import Dict, Optional
 from fastapi import FastAPI, HTTPException
 
 from app.models import UserRegister
-from app.user import UserRepository
+from app.user import add
 
 app = FastAPI(debug=True)
-db_user = UserRepository()
 
 
 @app.post("/register", status_code=201, response_model=UserRegister)
 def register_user(user: UserRegister) -> Optional[Dict[str, str]]:
-    id, comment, status_code = db_user.add(user.dict())
+    id, comment, status_code = add(user.dict())
 
     if id:
         return {"New user registered, ID:": str(id)}
