679e869591ea8abad1a8e6bb855b55136cb44341
Márcio Andrade <marcio.andrade@smarttbotter.com>
2022-02-19 12:16:09 -0300

    Extract fields.

--- app/models.py
+++ app/models.py
@@ -1,17 +1,21 @@
 from pydantic import BaseModel, Field
 
+_email_field = Field(regex=r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
+_name_field = Field(min_length=3, max_length=255)
+_password_field = Field(min_length=8, max_length=255)
+
 
 class UserRegister(BaseModel):
-    email: str = Field(regex=r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
-    name: str = Field(min_length=3, max_length=255)
-    password: str = Field(min_length=8, max_length=255)
+    email: str = _email_field
+    name: str = _name_field
+    password: str = _password_field
 
 
 class UserLogin(BaseModel):
-    email: str = Field(regex=r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
-    password: str = Field(min_length=8, max_length=255)
+    email: str = _email_field
+    password: str = _password_field
 
 
 class UserOutput(BaseModel):
     id: int
-    email: str
+    email: str = _email_field
