f695840cc142d5f17c8003c7d62a922889a7b78b
Márcio Andrade <marcio.andrade@smarttbotter.com>
2022-02-19 13:12:41 -0300

    Validate password on pydantic model.

--- app/models.py
+++ app/models.py
@@ -1,19 +1,38 @@
-from pydantic import BaseModel, Field
+import re
+
+from pydantic import BaseModel, Field, validator
 
 _email_field = Field(regex=r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
 _name_field = Field(min_length=3, max_length=255)
-_password_field = Field(min_length=8, max_length=255)
 
 
 class UserRegister(BaseModel):
     email: str = _email_field
     name: str = _name_field
-    password: str = _password_field
+    password: str = Field(min_length=8, max_length=255)
+
+    @validator("password")
+    def password_must_contain_upper_letter(cls, password: str) -> str:
+        if not bool(re.search(r"[A-Z]", password)):
+            raise ValueError("Password must contain an upper letter")
+        return password
+
+    @validator("password")
+    def password_must_contain_lower_letter(cls, password: str) -> str:
+        if not bool(re.search(r"[a-z]", password)):
+            raise ValueError("Password must contain a lower letter")
+        return password
+
+    @validator("password")
+    def password_must_contain_number(cls, password: str) -> str:
+        if not bool(re.search(r"[0-9]", password)):
+            raise ValueError("Password must contain a number")
+        return password
 
 
 class UserLogin(BaseModel):
     email: str = _email_field
-    password: str = _password_field
+    password: str
 
 
 class UserOutput(BaseModel):
--- app/user.py
+++ app/user.py
@@ -1,4 +1,3 @@
-import re
 from dataclasses import dataclass
 from typing import Literal
 
@@ -25,12 +24,10 @@ def add(user: UserRegister, session_maker: sessionmaker) -> AddSuccess | AddErro
     if verify_email_already_exists(user.email, session_maker):
         return AddError(reason="CONFLICT", message="Email already exists")
 
-    if not validate_password(user.password):
-        return AddError(reason="BAD_REQUEST", message="Invalid password")
-
-    password = encrypt_password(user.password)
     try:
-        user_add = User(name=user.name, email=user.email, password=password)
+        user_add = User(
+            name=user.name, email=user.email, password=encrypt_password(user.password)
+        )
         with session_maker() as session:
             session.add(user_add)
             session.commit()
@@ -45,17 +42,4 @@ def verify_email_already_exists(email_user: str, session_maker: sessionmaker) ->
 
 
 def encrypt_password(raw_password: str) -> str:
-    crypt_password = bcrypt.hashpw(
-        raw_password.encode("utf8"), bcrypt.gensalt(8)
-    ).decode()
-    return crypt_password
-
-
-def validate_password(password: str) -> bool:
-    if (
-        bool(re.search(r"[a-z]", password))
-        and bool(re.search(r"[A-Z]", password))
-        and bool(re.search(r"[1-9]", password))
-    ):
-        return True
-    return False
+    return bcrypt.hashpw(raw_password.encode("utf8"), bcrypt.gensalt(8)).decode()
